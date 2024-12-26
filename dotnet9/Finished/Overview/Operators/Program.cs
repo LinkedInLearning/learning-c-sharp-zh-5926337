d60601915c8f2dcaa683900051421edb13dbd54e
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-26 19:25:44 +1300

    新增示例项目，展示 C# 中的注释和运算符用法

---
+++ dotnet9/Finished/Overview/Operators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+// 声明一些变量来练习运算符的使用
+int x = 10, y = 5;
+string a = "abcd", b = "efgh";
+
+// 基本数学运算符 +, -, /, *
+Console.WriteLine("----- Basic Math -----");
+Console.WriteLine((x / y) * x);
+Console.WriteLine(a + b);
+
+// 增量和减量运算符 ++, --
+Console.WriteLine("----- Shorthand -----");
+
+// TODO: 后缀递增和递减运算符。x++ 和 x-- 会先返回 x 的值，然后再递增或递减 x
+x++;
+y--;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+// TODO: 前缀递增和递减运算符。++x 和 --x 会先递增或递减 x，然后再返回 x
+
+
+// 加法赋值运算符 a = a + b 可以写成 a += b
+a += b;
+Console.WriteLine(a);
+
+// 减法赋值运算符 a = a - b 可以写成 a -= b
+x -= y;
+Console.WriteLine(x);
+
+// 逻辑运算符 && 和 ||
+Console.WriteLine("----- Logic Operators -----");
+Console.WriteLine(x > y && y >= 5);
+Console.WriteLine(x > y || y >= 5);
+
+// null-coalescing operators
+string str = null;
+// the ?? operator uses left operand if not null, or right one if it is
+Console.WriteLine(str ?? "Unknown string");
+
+// the ??= operator assigns the right operand if the left one is null
+// it replaces the code:
+// if (variable is null) {
+//    variable = somevalue;
+// }
+str ??= "New string";
+Console.WriteLine(str);
---
+++ dotnet9/Start/Overview/Operators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// 声明一些变量来练习运算符的使用
+int x = 10, y = 5;
+string a = "abcd", b = "efgh";
+
+// TODO: 基本数学运算符 +, -, /, *
+Console.WriteLine("----- Basic Math -----");
+
+
+// TODO: 增量和减量运算符 ++, --
+Console.WriteLine("----- Shorthand -----");
+
+// TODO: 后缀递增和递减运算符。x++ 和 x-- 会先返回 x 的值，然后再递增或递减 x
+
+
+// TODO: 前缀递增和递减运算符。++x 和 --x 会先递增或递减 x，然后再返回 x
+
+
+// TODO: 加法赋值运算符 a = a + b 可以写成 a += b
+
+
+// TODO: 减法赋值运算符 a = a - b 可以写成 a -= b
+
+
+// TODO: 逻辑运算符 && 和 ||
+Console.WriteLine("----- Logic Operators -----");
+
+
+// null合并运算符
+string str = null;
+// TODO: 如果左操作数的值不为 null，那 ?? 运算符就会返回该值，否则返回右操作数的值
+
+
+// TODO: 如果左操作数的值为 null，那么 ??= 运算符就会将右操作数的值赋给左操作数
+// 可以用来代替这种代码：
+// if (variable is null) {
+//    variable = somevalue;
+// }
+
+// TODO: 三元条件运算符 ?:，如果条件为真，返回冒号左边的值，否则返回右边的值
+
