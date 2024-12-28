5252f71368d49222498ba59b9c6ea556e23a56cb
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-28 17:11:38 +1300

    Enhance project settings and error handling in Program.cs
    Updated Exceptions.csproj to enable implicit usings and nullable reference types.
    Modified Program.cs to improve exception handling clarity, added a new variable, and adjusted output messages.
    Moved division operation outside of the try block to change error handling behavior.

--- dotnet9/Finished/Program Flow/Exceptions/Program.cs
+++ dotnet9/Finished/Program Flow/Exceptions/Program.cs
@@ -1,30 +1,28 @@
-﻿using System;
-
-
-int x = 100;
+﻿int x = 100;
 int y = 10;
 int result;
 
+// try-catch 表达式使错误检查更容易
 try
 {
     if (x > 1000)
     {
-        throw new ArgumentOutOfRangeException("x", "x 必须小于或等于 1000。");
+        throw new ArgumentOutOfRangeException(nameof(x), "x 必须小于或等于 1000");
     }
     result = x / y;
     Console.WriteLine("结果是: {0}", result);
 }
 catch (DivideByZeroException e)
 {
-    Console.WriteLine("出错了！你不能除以 0。");
+    Console.WriteLine("发生了一个错误");
     Console.WriteLine(e.Message);
 }
 catch (ArgumentOutOfRangeException e)
 {
-    Console.WriteLine("抱歉, x 的值不能超过 1000！");
+    Console.WriteLine("抱歉, x 的值不能超过 1000");
     Console.WriteLine(e.Message);
 }
 finally
 {
-    Console.WriteLine("这段代码始终会执行。");
+    Console.WriteLine("这段代码始终会执行");
 }
