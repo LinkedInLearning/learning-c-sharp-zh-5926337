55c265222dc7725dedc661142afa94d7008b60ea
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-30 20:26:58 +1300

    Update project to .NET 9 and enhance tuple examples
    Updated `MultiValues.csproj` to target .NET 9 SDK with appropriate properties. Enhanced `Program.cs` with examples and comments on tuples, including creation, access, and function returns, along with TODO notes for clarity.

---
+++ dotnet9/Finished/Functions/MultiValues/Program.cs
@@ -0,0 +1,28 @@
+﻿// Tuple 是一种组合值的数据结构，可以将多个值组合在一起，从 C# 7 开始引入
+(int a, int b) tup1 = (5, 10);
+var tup2 = ("Hello", 3.14);
+
+// Tuple 可以包含多个值
+(int price, int quantity, string name) tup3 = (10, 5, "Apple");
+
+// Tuple 值是可变的
+tup1.b = 20;
+tup2.Item1 = "你好";
+
+Console.WriteLine($"{tup1.a}, {tup1.b}");
+Console.WriteLine($"{tup2.Item1},{tup2.Item2}");
+Console.WriteLine($"{tup3.price}, {tup3.quantity}, {tup3.name}");
+
+// 函数可以使用元组返回多个值
+(int sum, int product) PlusTimes(int a, int b)
+{
+    return (a + b, a * b);
+}
+
+// 调用函数并获取元组的值
+(int, int) result = PlusTimes(6, 12);
+Console.WriteLine($"Sum: {result.Item1}, Product: {result.Item2}");
+var result2 = PlusTimes(8, 15);
+Console.WriteLine($"Sum: {result2.sum}, Product: {result2.product}");
+
+
