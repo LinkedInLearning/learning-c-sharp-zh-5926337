7b43fc8d66e1e7fbc5cd121c4853328efc6f5eed
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-30 15:42:30 +1300

    Update culture settings and project properties
    Modified `Program.cs` to set the current thread's culture to Chinese (zh-CN) and updated output formatting for mileage and price. Clarified comments and removed unnecessary TODOs.
    
    In `Strings.csproj`, added `ImplicitUsings` and `Nullable` properties to enhance code safety and reduce boilerplate.

--- dotnet9/Finished/Strings/Interpolation/Program.cs
+++ dotnet9/Finished/Strings/Interpolation/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-
+﻿using System.Globalization;
 // 设置当前线程的区域性为中文
-System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("zh-CN");
+//CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("zh-CN");
 
 // 声明一些变量
 string make = "Mercedes-Benz";
@@ -11,11 +10,12 @@ float miles = 8_450.27f;
 decimal price = 60_275.0m;
 
 // Output information using formatting
-Console.WriteLine("这辆车是 {0} {1} {2}, 已行驶 {3} 公里， 价格是 ￥{4}",
+Console.WriteLine("这辆车是 {0} {1} {2}, 已行驶 {3} 英里， 价格是 ${4}",
     year, make, model, miles, price);
 
 // 使用字符串插值
-Console.WriteLine($"这辆车是 {year} {make} {{{model}}}, 已行驶 {miles} 英里，价格是 {price:C2}");
+Console.WriteLine($"这辆车是 {year} {make} {model}, 已行驶 {miles} 英里，价格是 {price.ToString("C2", new CultureInfo("zh-CN"))}");
 
 // 使用内联表达式
-Console.WriteLine($"这辆车是 {year} {make} {{{model}}}, 已行驶 {miles * 1.6:F2} 公里，价格是 {price:C2}");
+Console.WriteLine($"这辆车是 {year} {make} {model}, 已行驶 {{{miles * 1.6:F2}}} 公里，价格是 {price.ToString("C2", new CultureInfo("zh-CN"))}");
+
--- dotnet9/Start/Strings/Interpolation/Program.cs
+++ dotnet9/Start/Strings/Interpolation/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-
-
-// 声明一些变量
+﻿// 声明一些变量
 string make = "Mercedes-Benz";
 string model = "G Class";
 int year = 2020;
 float miles = 8_450.27f;
 decimal price = 60_275.0m;
 
-// TODO: 使用格式化输出信息
-
+// 使用字符串格式化输出信息
 
 // TODO: 使用字符串插值
 
-
 // TODO: 使用内联表达式
-
