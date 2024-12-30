5d313a557bedfb7d3b2f3ca10237cad0f2a30382
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-31 12:30:01 +1300

    Enhance parameter handling in TestFunc methods
    - Updated `TestFunc1` to demonstrate value passing by modifying `arg1` and printing its value.
    - Modified `TestFunc2` to show reference passing with `ref`, allowing changes to `arg1` to reflect in the caller.
    - Implemented logic in `PlusTimes` to calculate and return sum and product using `out` parameters.

--- dotnet9/Finished/Functions/RefAndOutParams/Program.cs
+++ dotnet9/Finished/Functions/RefAndOutParams/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 一般的值参数不能被函数修改，因为它们是原始值的副本
-static void TestFunc1(int arg1)
+void TestFunc1(int arg1)
 {
     arg1 += 10;
     Console.WriteLine($"在 TestFunc1 内部：{arg1}");
@@ -7,7 +7,7 @@ static void TestFunc1(int arg1)
 
 
 // 以引用方式传递的参数可以被函数修改，并反映到调用者
-static void TestFunc2(ref int arg1)
+void TestFunc2(ref int arg1)
 {
     arg1 += 10;
     Console.WriteLine($"在 TestFunc2 内部：{arg1}");
@@ -15,7 +15,7 @@ static void TestFunc2(ref int arg1)
 
 
 // out 关键字表示参数返回一个值，而不是用于向函数提供数据
-static void PlusTimes(int arg1, int arg2, out int sum, out int product)
+void PlusTimes(int arg1, int arg2, out int sum, out int product)
 {
     sum = arg1 + arg2;
     product = arg1 * arg2;
