b3c8d5ad887e65f5124209dad7582949a1728d8f
yanxiaodi <yan_xiaodi@hotmail.com>
2024-12-30 15:44:55 +1300

    Update project to .NET 9.0 and add PrintWithPrefix function
    - Updated `Params.csproj` to target .NET 9.0 SDK with necessary properties.
    - Added `PrintWithPrefix` function in `Program.cs` with a default parameter.
    - Included example calls demonstrating default and named parameters.
    - Noted potential duplication in `Params.csproj` changes.
    - Added TODO comments for further implementation of parameter examples.

---
+++ dotnet9/Finished/Functions/DefaultParams/Program.cs
@@ -0,0 +1,12 @@
+﻿// 函数可以为其参数提供默认值
+void PrintWithPrefix(string value, string prefix = "")
+{
+    Console.WriteLine($"{prefix} {value}");
+}
+
+// 使用默认参数
+PrintWithPrefix("你好！");
+PrintWithPrefix("你好！", ">: ");
+
+// 使用命名参数
+PrintWithPrefix(prefix: "% ", value: "你好！");
---
+++ dotnet9/Start/Functions/DefaultParams/Program.cs
@@ -0,0 +1,11 @@
+﻿// TODO: 函数可以为其参数提供默认值
+void PrintWithPrefix(string value, string prefix)
+{
+    Console.WriteLine($"{prefix} {value}");
+}
+
+// TODO: 使用默认参数
+
+
+// TODO: 使用命名参数
+
