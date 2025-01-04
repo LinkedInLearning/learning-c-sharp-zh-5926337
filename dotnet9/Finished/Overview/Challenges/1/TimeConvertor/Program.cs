0eb2dd1c60531e7b0f21229f56d04410c2b9d1c4
yanxiaodi <yan_xiaodi@hotmail.com>
2025-01-05 12:17:11 +1300

    Add Time Conversion project and update instructions
    Added a new section in `Instruction.md` titled "时间转换" (Time Conversion) with task details and test cases. Created a new Visual Studio solution `TimeConvertor.sln` and project file `TimeConvertor.csproj` for the "TimeConvertor" project targeting net9.0. Added initial C# program in `Program.cs` with a placeholder message and time conversion logic. Updated `Instruction.md` with an example output. Re-added solution and project files with the same settings.

---
+++ dotnet9/Finished/Overview/Challenges/1/TimeConvertor/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("输入秒数：");
+int totalSeconds = int.Parse(Console.ReadLine());
+
+// 实现你的逻辑
+int hours = totalSeconds / 3600;
+int minutes = (totalSeconds % 3600) / 60;
+int seconds = totalSeconds % 60;
+
+Console.WriteLine("{0} hour(s), {1} minute(s), and {2} second(s)", hours, minutes, seconds);
+// 你也可以使用下面的方式输出，这是字符串插值的写法，我们会在后面的章节中详细讲解
+// Console.WriteLine("{0} hour(s), {1} minute(s), and {2} second(s)", hours, minutes, seconds);
+
+
---
+++ dotnet9/Start/Overview/Challenges/1/TimeConvertor/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("输入秒数：");
+int totalSeconds = int.Parse(Console.ReadLine());
+
+// 在下面实现你的逻辑
