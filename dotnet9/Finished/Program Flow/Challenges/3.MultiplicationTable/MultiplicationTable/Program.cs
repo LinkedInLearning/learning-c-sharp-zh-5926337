1addcc7ee9c0bd308619bdff06b6c8026599218b
yanxiaodi <yan_xiaodi@hotmail.com>
2025-01-05 14:15:25 +1300

    Add multiplication table project and update files
    Updated solution and project files for Visual Studio 2022.
    Implemented a console application in `Program.cs` to generate
    and display a multiplication table from 1 to 9. Enhanced
    `README.md` with program description and usage hints.

---
+++ dotnet9/Finished/Program Flow/Challenges/3.MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 1; i <= 9; i++) // 外层循环控制行数
+{
+  for (int j = 1; j <= i; j++) // 内层循环控制每行的列数
+  {
+    Console.Write("{0} * {1} = {2}\t", j, i, i * j); // 输出乘法表达式，用 \t 对齐
+  }
+  Console.WriteLine(); // 每行结束换行
+}
