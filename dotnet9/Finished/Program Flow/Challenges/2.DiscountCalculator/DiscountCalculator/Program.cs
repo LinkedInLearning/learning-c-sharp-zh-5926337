b40673a6cf2a4830c931f050db50f8410dcc7a05
yanxiaodi <yan_xiaodi@hotmail.com>
2025-01-05 14:10:35 +1300

    Add Discount Calculator project and implementation
    Created a new Visual Studio solution and project for the Discount Calculator. Implemented a console application that calculates discounts based on user input and outputs results in Chinese. Added a README file with program requirements and examples.

---
+++ dotnet9/Finished/Program Flow/Challenges/2.DiscountCalculator/DiscountCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 在下面实现你的逻辑
+
+Console.WriteLine("请输入购买金额：");
+// 对涉及到货币的计算，建议使用 decimal 类型
+// 用 decimal.Parse 方法将用户输入的字符串转换为 decimal 类型
+var amount = decimal.Parse(Console.ReadLine());
+var discount = 0.0m;
+
+if (amount >= 500)
+{
+  discount = amount * 0.2m; // 20% 折扣
+}
+else if (amount >= 200)
+{
+  discount = amount * 0.1m; // 10% 折扣
+}
+// 小于 200 时，折扣为 0，不需要处理
+
+var finalAmount = amount - discount;
+// 输出结果
+Console.WriteLine("折扣金额：{0}", discount);
+Console.WriteLine("最终支付金额：{0}", finalAmount);
---
+++ dotnet9/Start/Program Flow/Challenges/2.DiscountCalculator/DiscountCalculator/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 在下面实现你的逻辑
