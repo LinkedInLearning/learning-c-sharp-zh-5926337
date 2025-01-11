08d91cd02ca671bb511db28cd0112bfd3a11e803
yanxiaodi <yan_xiaodi@hotmail.com>
2025-01-12 11:33:55 +1300

    Add a product sample project

---
+++ dotnet9/Finished/Classes/Challenges/1.ProductSample/ProductSample/Product.cs
@@ -0,0 +1,112 @@
+﻿namespace ProductSample;
+
+/// <summary>
+/// 产品类
+/// </summary>
+internal class Product
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new InvalidOperationException("名称不能为空");
+            }
+            _name = value;
+        }
+    }
+
+    private string _category;
+    public string Category
+    {
+        get => _category;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new InvalidOperationException("类别不能为空");
+            }
+            _category = value;
+        }
+    }
+    public string Description { get; set; }
+
+    private decimal _price;
+
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new InvalidOperationException("价格不能小于或等于0");
+            }
+            _price = value;
+        }
+    }
+
+    private int _stock;
+    public int Stock
+    {
+        get => _stock;
+        set
+        {
+            if (value < 0)
+            {
+                throw new InvalidOperationException("库存不能小于0");
+            }
+            _stock = value;
+        }
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="name">商品名称</param>
+    /// <param name="category">类别</param>
+    /// <param name="description">描述</param>
+    /// <param name="price">单价</param>
+    /// <param name="stock">库存</param>
+    public Product(string name, string category, string description, decimal price, int stock)
+    {
+        Name = name;
+        Category = category;
+        Description = description;
+        Price = price;
+        Stock = stock;
+    }
+
+    public void Sell(int count)
+    {
+        if (count <= 0)
+        {
+            throw new InvalidOperationException("销售数量不能小于或等于0");
+        }
+        if (Stock < count)
+        {
+            throw new InvalidOperationException("库存不足");
+        }
+        Stock -= count;
+        Console.WriteLine($"成功售出 {count} 个{Name}，库存剩余 {Stock} 个");
+    }
+
+    public void Restock(int count)
+    {
+        if (count <= 0)
+        {
+            throw new InvalidOperationException("进货数量不能小于或等于0");
+        }
+
+        Stock += count;
+        Console.WriteLine($"成功进货 {count} 个{Name}，库存剩余 {Stock} 个");
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}（{Category}）, {Description}, 单价: {Price:C}, 库存: {Stock}";
+    }
+}
---
+++ dotnet9/Finished/Classes/Challenges/1.ProductSample/ProductSample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ProductSample;
+
+Console.WriteLine("Hello, World!");
+
+// 创建商品对象
+var product = new Product("笔记本电脑", "电子产品", "ThinkPad X1 Carbon", 7999, 100);
+Console.WriteLine(product.ToString());
+
+// 销售商品
+product.Sell(10);
+Console.WriteLine(product.ToString());
+
+// 进货商品
+product.Restock(20);
+Console.WriteLine(product.ToString());
